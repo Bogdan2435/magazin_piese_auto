2138c854974974a3db6743c5499dbe18e862cfd9
Dura Bogdan <73533187+Bogdan2435@users.noreply.github.com>
2022-05-11 13:28:25 +0300

    add piesa and model masina admin inline

--- crud app/magpiese/tabele/admin.py
+++ crud app/magpiese/tabele/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 
-from tabele.models import Adrese, Angajati, Clienti, LocuriMunca, Modele_Masini, Piese
+from tabele.models import Adrese, Angajati, Clienti, LocuriMunca, Modele_Masini, Piesa_Model, Piese
 
-# Register your models here.
+class PiesaModelInline(admin.TabularInline):
+    model = Piesa_Model
+    extra = 1
+
+class PieseAdmin(admin.ModelAdmin):
+    inlines = (PiesaModelInline, )
+    model = Piese
+
+class ModeleMasiniAdmin(admin.ModelAdmin):
+    inlines = (PiesaModelInline, )
+    model = Modele_Masini
 
 admin.site.register(LocuriMunca)
 admin.site.register(Angajati)
 admin.site.register(Adrese)
 admin.site.register(Clienti)
-admin.site.register(Modele_Masini)
-admin.site.register(Piese)
+admin.site.register(Modele_Masini, ModeleMasiniAdmin)
+admin.site.register(Piese, PieseAdmin)
+# admin.site.register(Piesa_Model)
