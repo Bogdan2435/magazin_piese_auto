73bd89a4d447e34477ab800be3cb50206c6ffb8a
Dura Bogdan <73533187+Bogdan2435@users.noreply.github.com>
2022-05-24 15:50:44 +0300

    change home, 3c&d, search

--- magpiese/tabele/forms.py
+++ magpiese/tabele/forms.py
@@ -5,8 +5,8 @@ from .models import *
 class LocMuncaForm(forms.ModelForm):
 
     denumire = forms.CharField(widget = forms.TextInput(attrs = {'class': 'form-control form-control-lg'}))
-    salariu_min = forms.IntegerField(widget = forms.TextInput(attrs = {'class': 'form-control form-control-lg'}))
-    salariu_max = forms.IntegerField(widget = forms.TextInput(attrs = {'class': 'form-control form-control-lg'}))
+    salariu_min = forms.IntegerField(widget = forms.NumberInput(attrs = {'class': 'form-control form-control-lg'}))
+    salariu_max = forms.IntegerField(widget = forms.NumberInput(attrs = {'class': 'form-control form-control-lg'}))
     
     class Meta:
         model = LocuriMunca
@@ -15,9 +15,31 @@ class LocMuncaForm(forms.ModelForm):
 
 
 class AngajatiForm(forms.ModelForm):
+
+    nume_familie = forms.CharField(widget = forms.TextInput(attrs = {'class': 'form-control form-control-lg'}))
+    prenume = forms.CharField(widget = forms.TextInput(attrs = {'class': 'form-control form-control-lg'}))
+    salariu = forms.IntegerField(widget = forms.NumberInput(attrs = {'class': 'form-control form-control-lg'}))
+    email = forms.CharField(widget = forms.TextInput(attrs = {'class': 'form-control form-control-lg'}))
+    data_angajare = forms.DateField(widget = forms.DateInput(attrs = {'class': 'form-control form-control-lg', 'data-toggle': 'datepicker-icon'}))
+    data_nasterii = forms.DateField(widget = forms.DateInput(attrs = {'class': 'form-control form-control-lg', 'data-toggle': 'datepicker-icon'}))
+    # loc_munca = forms.ChoiceField(queryset=LocuriMunca.objects.all(), widget = forms.Select(attrs = {'class': 'form-control form-control-lg'}))
+
     class Meta:
         model = Angajati
         fields = "__all__"
+
+    widget = {
+        'loc_munca': forms.Select(attrs = {'class': 'form-control form-control-lg'})
+    }
+
+    # widgets = {
+    #     'nume_familie': forms.TextInput(attrs = {'class': 'form-control form-control-lg'}),
+    #     'prenume': forms.TextInput(attrs = {'class': 'form-control form-control-lg'}),
+    #     'salariu': forms.NumberInput(attrs = {'class': 'form-control form-control-lg'}),
+    #     'email': forms.TextInput(attrs = {'class': 'form-control form-control-lg'}),
+    #     'data_angajare': forms.DateInput(attrs = {'class': 'form-control form-control-lg'}),
+    #     'data_nasterii': forms.DateInput(attrs = {'class': 'form-control form-control-lg'}),
+    # }
 
 
 class ClientiForm(forms.ModelForm):
